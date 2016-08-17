81211d136c8f4dad7c1feb168db5fee7cf97ae06
thomas <Thomas.Schroedter@gmx.net>
2016-08-17 17:04:07 +1000

    Updated from private repository

---
+++ Selkie.Services.Common.Dto/SurveyGeoJsonFeatureDto.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+
+namespace Selkie.Services.Common.Dto
+{
+    [UsedImplicitly]
+    public class SurveyGeoJsonFeatureDto
+    {
+        [NotNull]
+        public string SurveyFeatureAsGeoJson = string.Empty;
+
+        [NotNull]
+        public SurveyFeatureDto SurveyFeatureDto = new SurveyFeatureDto();
+    }
+}
